043ea98fb19e5a99566fd1b02ff8a54669e9794b
sabashub <shubitidzesaba09@gmail.com>
2024-01-25 20:56:07 +0400

    initialize project

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & Share</h1>
+      <br className="max-md:hidden" />
+      <span>AI_Powered Prompts</span>
+    </section>
+  );
+};
+
+export default Home;
